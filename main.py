0a1cc8675956679e16f67f51088eb621222c6c93
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2021-09-03 18:53:41 -0300

    importando o controle de Webcam

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)
+
+if webcam.isOpened():
+    validacao, frame = webcam.read()
+    while validacao:
+        validacao, frame = webcam.read()
+        cv2.imshow('Vídeo da Webcam', frame)
+        key = cv2.waitKey(5)
+        if key == 27: #ESC
+            break
+    cv2.imwrite('Foto.png', frame)
+
+webcam.release()
+cv2.destroyAllWindows()
